19ae3d629bfa8d1e69992248542bd2c7fe696f13
Ben Farmer <ben.farmer@gmail.com>
2018-11-19 10:42:48 +0000

    Added script to automate stripping of ScannerBit tarball

---
+++ grab_ScannerBit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# A simple script to grab the ScannerBit standalone tarball from GAMBIT,
+# strip out some junk we don't need, and move it into the pyScannerBit
+# package
+
+GAMBIT_DIR=$1
+
+echo "Grabbing ScannerBit tarball from $1/build..."
+cp $GAMBIT_DIR/build/ScannerBit-1.1.3.tar pyscannerbit/scannerbit/ScannerBit_TEMP.tar
+mkdir pyscannerbit/scannerbit/ScannerBit_TEMP_dir
+echo "Untarring..."
+tar -C pyscannerbit/scannerbit/ScannerBit_TEMP_dir -xf pyscannerbit/scannerbit/ScannerBit_TEMP.tar --strip-components=1 
+
+CONTRIB_DIR=pyscannerbit/scannerbit/ScannerBit_TEMP_dir/contrib/
+
+echo "Stripping unnecessary contrib packages..."
+rm -rf $CONTRIB_DIR/Delphes*  
+rm -rf $CONTRIB_DIR/heputils     
+rm -rf $CONTRIB_DIR/mcutils  
+rm -rf $CONTRIB_DIR/pybind11      
+rm -rf $CONTRIB_DIR/RestFrames*        
+rm -rf $CONTRIB_DIR/slhaea
+rm -rf $CONTRIB_DIR/fjcore*   
+rm -rf $CONTRIB_DIR/MassSpectra  
+rm -rf $CONTRIB_DIR/pyscannerbit  
+rm -rf $CONTRIB_DIR/restframes*.tar.gz  
+rm -rf $CONTRIB_DIR/yaml-cpp*
+
+echo "Recreating tarball..."
+rm pyscannerbit/scannerbit/ScannerBit_stripped.tar
+tar -C pyscannerbit/scannerbit -cf pyscannerbit/scannerbit/ScannerBit_stripped.tar ScannerBit_TEMP_dir
+echo "Deleting temporary files..."
+rm -rf pyscannerbit/scannerbit/ScannerBit_TEMP_dir
+rm pyscannerbit/scannerbit/ScannerBit_TEMP.tar 
+echo "Done!"
